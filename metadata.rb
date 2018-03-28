5c28d3c1fa4c215a6402acafc3ac94d45420a736
hadl1342 <charlotte@uidaho.edu>
2018-03-28 22:31:54 +0000

    added template for Default.html and updated version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures myiis'
 long_description 'Installs/Configures myiis'
-version '0.2.0'
+version '0.2.1'
 
 # test
 # The `issues_url` points to the location where issues for this cookbook are
