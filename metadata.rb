6eea145989fa3fd0d62d54804b1ffaa69b393e3a
hadl1342 <charlotte@uidaho.edu>
2018-03-28 21:43:25 +0000

    version 0.2.0 added system info to default.htm

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures myiis'
 long_description 'Installs/Configures myiis'
-version '0.1.1'
+version '0.2.0'
 
 # test
 # The `issues_url` points to the location where issues for this cookbook are
