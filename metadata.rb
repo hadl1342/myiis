90bcd0690000b830eb929998c1960f0c4f3e4180
hadl1342 <charlotte@uidaho.edu>
2018-03-28 21:08:37 +0000

    testing commit

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description 'Installs/Configures myiis'
 long_description 'Installs/Configures myiis'
 version '0.1.1'
 
+# test
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
